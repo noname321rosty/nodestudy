7e928753609728956c13f181ef8bdb2b72a9ed4f
noname321rosty <bilka2001@gmail.com>
2020-12-02 19:33:26 +0200

    fix / , /query url

--- hw3/core/controllers/users/users.controller.js
+++ hw3/core/controllers/users/users.controller.js
@@ -2,22 +2,10 @@ const {usersService} = require("../../services");
 
 module.exports = {
     getAllUsers: (req, res) => {
-        const {email, name} = req.query;
+        const user = usersService.getUsers(req.query);
 
-        if (req.query){
-            const user = usersService.getUsers(email, name);
-            res.status(400).json(user);
-        }
-
-        const users = usersService.getUsers();
-        res.status(400).json(users);
+        res.status(400).json(user);
     },
-    // getUserByEmailOrName: (req, res) => {
-    //     const {email, name} = req.query;
-    //     const user = usersService.getUsers(email, name);
-    //
-    //     res.status(400).json(user);
-    // },
     createUser: (req, res) => {
         const user = req.body;
         const createdUser = usersService.createUser(user);
