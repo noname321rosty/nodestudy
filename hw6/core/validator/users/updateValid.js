a18ea2ae1bc7db2c703491791128530a71b2e751
noname321rosty <bilka2001@gmail.com>
2020-12-15 20:15:50 +0200

    fix way

--- hw6/core/validator/users/updateValid.js
+++ hw6/core/validator/users/updateValid.js
@@ -1,6 +1,6 @@
 const valid = require('joi');
 
-const {email, password}  = require('../../statuses');
+const {email, password}  = require('../../statuses/regex.enum');
 
 module.exports = valid.object().keys({
     email: valid.string().regex(email).trim().required(),
